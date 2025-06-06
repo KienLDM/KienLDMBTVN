67511070a688d6ea0b5602819c2a022daef2a3ab
KienLDM <kienldm@apero.vm>
2025-06-06 18:32:32 +0700

    Define design system

--- app/src/main/java/com/example/kienldmbtvn/ui/theme/Color.kt
+++ app/src/main/java/com/example/kienldmbtvn/ui/theme/Color.kt
@@ -1,11 +1,18 @@
 package com.example.kienldmbtvn.ui.theme
 
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val primaryTextColor = Color (0xFFE400D9)
+val primaryBackgroundColor = Color (0xFFFFFFFF)
+val primaryBorderColor = Color (0xFFE400D9)
+val normalTextColor = Color (0xFF000000)
+val chosenTextColor = Color (0x1D00F5E0)
+val startGradientColor = Color (0xFFE400D9)
+val endGradientColor = Color (0xFF1D00F5)
+val buttonBackground = Brush.linearGradient(
+    colors = listOf(
+        startGradientColor,
+        endGradientColor
+    )
+)
