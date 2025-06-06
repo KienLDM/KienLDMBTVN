17db4b237277c11ea1238a6394fff9a7a56600ea
KienLDM <kienldm@apero.vm>
2025-06-06 18:34:53 +0700

    Added Style screen

---
+++ app/src/main/java/com/example/kienldmbtvn/ui/style/StyleScreen.kt
@@ -0,0 +1,15 @@
+package com.example.kienldmbtvn.ui.style
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StyleScreen() {
+    StyleComponents("https://images.unsplash.com/photo-1575936123452-b67c3203c357?fm=jpg&q=60&w=3000&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D")
+}
+
+@Preview
+@Composable
+private fun StyleScreenPreview() {
+    StyleScreen()
+}
