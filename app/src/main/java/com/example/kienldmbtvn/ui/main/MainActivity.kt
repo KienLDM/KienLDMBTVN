482712f27dde8a81a7b414d89e27ec84cd511630
KienLDM <kienldm@apero.vm>
2025-06-10 17:10:46 +0700

    Added new colors and hide system bar extension

--- app/src/main/java/com/example/kienldmbtvn/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/kienldmbtvn/ui/main/MainActivity.kt
@@ -4,12 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.kienldmbtvn.ext.hideSystemBar
 import com.example.kienldmbtvn.ui.style.StyleScreen
 import com.example.kienldmbtvn.ui.theme.KienLDMBTVNTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        hideSystemBar(
+            hideStatusBar = true,
+            hideNavigationBar = true,
+            isLightStatusBar = false
+        )
         enableEdgeToEdge()
         setContent {
             KienLDMBTVNTheme {
--- app/src/main/java/com/example/kienldmbtvn/ui/theme/Theme.kt
+++ app/src/main/java/com/example/kienldmbtvn/ui/theme/Theme.kt
@@ -9,13 +9,16 @@ import androidx.compose.ui.graphics.Brush
 
 private val customColorScheme = CustomColorScheme(
     primaryTextColor = primaryTextColor,
+    secondaryTextColor = secondaryTextColor,
     primaryBackgroundColor = primaryBackgroundColor,
+    errorBackgroundColor = errorBackgroundColor,
     primaryBorderColor = primaryBorderColor,
     normalTextColor = normalTextColor,
     chosenTextColor = chosenTextColor,
     startGradientColor = startGradientColor,
     endGradientColor = endGradientColor,
     buttonBackground = buttonBackground,
+    promptTextColor = promptTextColor
 )
 
 val LocalCustomColors = compositionLocalOf { customColorScheme }
@@ -33,7 +36,6 @@ fun KienLDMBTVNTheme(
     ) {
         MaterialTheme(
             colorScheme = materialColorScheme,
-            typography = Typography,
             content = content
         )
     }
@@ -41,11 +43,14 @@ fun KienLDMBTVNTheme(
 
 data class CustomColorScheme(
     val primaryTextColor: androidx.compose.ui.graphics.Color,
+    val secondaryTextColor: androidx.compose.ui.graphics.Color,
     val primaryBackgroundColor: androidx.compose.ui.graphics.Color,
+    val errorBackgroundColor: androidx.compose.ui.graphics.Color,
     val primaryBorderColor: androidx.compose.ui.graphics.Color,
     val normalTextColor: androidx.compose.ui.graphics.Color,
     val chosenTextColor: androidx.compose.ui.graphics.Color,
     val startGradientColor: androidx.compose.ui.graphics.Color,
     val endGradientColor: androidx.compose.ui.graphics.Color,
+    val promptTextColor: androidx.compose.ui.graphics.Color,
     val buttonBackground: Brush,
 )
