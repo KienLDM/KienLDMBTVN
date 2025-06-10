3705c41caaefe246dabc15f2cb17cbf3d3dd6a48
KienLDM <kienldm@apero.vm>
2025-06-10 11:36:45 +0700

    Initialize Koin

---
+++ app/src/main/java/com/example/kienldmbtvn/di/AppModule.kt
@@ -0,0 +1,16 @@
+package com.example.kienldmbtvn.di
+
+import com.example.kienldmbtvn.data.style.StyleApiClient
+import com.example.kienldmbtvn.data.style.StyleApiService
+import com.example.kienldmbtvn.data.style.StyleRepository
+import com.example.kienldmbtvn.data.style.StyleRepositoryImpl
+import com.example.kienldmbtvn.ui.style.StyleViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val appModule = module {
+    single<StyleApiService> { StyleApiClient.api }
+    single<StyleRepository> { StyleRepositoryImpl(get()) }
+
+    viewModelOf(::StyleViewModel)
+}
